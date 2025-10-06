63d9d789f57e2d914104c338582e867193d639db
DBedis <dbedis@ymail.com>
2025-10-06 17:17:30 +0100

    route edited

--- routes/SessionRoutes.js
+++ routes/SessionRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const GameSession = require('./model/GameSession');
+const GameSession = require('../model/GameSession');
 const Player = require('./model/PlayersContact');
 const mongoose = require('mongoose');
 
